0f2fba2a4b0b615c84511b80c6ea7218e35c3a37
Freddy Orozco <freddy.orozco@patagonian.it>
2021-08-11 16:16:07 -0500

    aug_09: Add navigation config

--- android/app/src/main/java/com/cursorn/MainActivity.java
+++ android/app/src/main/java/com/cursorn/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cursorn;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "cursorn";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
