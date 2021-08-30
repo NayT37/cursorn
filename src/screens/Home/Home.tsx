e934a11ed8b91a321db3023f5b2382a643a5a99b
Freddy Orozco <freddy.orozco@patagonian.it>
2021-08-30 16:07:46 -0500

    aug_25: Add books data to home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // Components
 import {MainHeader, Typography, DefaultButton} from '../../components';
-import {View} from 'react-native';
+import {Alert, View} from 'react-native';
 // Resources
 import styles from './styles';
+import {getAllBooks} from '../../services';
 import {goToScreen} from '../../navigation/controls';
 
 const goToExperimentalScreen = () => {
@@ -11,12 +12,37 @@ const goToExperimentalScreen = () => {
 };
 
 const HomeScreen = () => {
+    const [Books, setBooks] = useState([]);
+
+    /**
+     * Called each time component renders
+     * Do not receive async functions
+     */
+    useEffect(() => {
+        // dis from dispatch
+        const dis = async () => {
+            try {
+                const {success, data} = await getAllBooks();
+                if (success) {
+                    setBooks(data);
+                } else {
+                    Alert.alert('error getting books on Home screen');
+                }
+            } catch (error) {
+                console.error(error);
+                Alert.alert('error getting books on Home screen');
+            }
+        };
+        dis();
+    }, []);
+
     return (
         <>
             <MainHeader title="Home" hideGoBack />
             <View style={styles.mainContainer}>
                 <Typography>Home Page</Typography>
                 <DefaultButton text="Go to experimental screen" onPress={goToExperimentalScreen} />
+                <Typography>{JSON.stringify(Books, null, 2)}</Typography>
             </View>
         </>
     );
